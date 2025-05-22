617f4eedc7fa4f01cf3918169670778ff671cc50
Kichang Park <45839902+lovepkc@users.noreply.github.com>
2025-05-22 16:25:41 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+import chat
+import db
+
+st.markdown(db.FOOTER, unsafe_allow_html=True)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role":"system", "content":db.INSTRUCTION}]
+
+for msg in st.session_state.messages[1:]:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+user_input = st.chat_input("메시지를 입력하세요...")
+
+if user_input:
+    st.session_state.messages.append({"role":"user", "content":user_input})
+
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    with st.chat_message("assistant"):
+        with st.spinner("메시지 작성 중..."):
+            reply = chat.get_reply(user_input)
+            st.markdown(reply)
+            st.session_state.messages.append({"role":"assistant", "content":reply})
